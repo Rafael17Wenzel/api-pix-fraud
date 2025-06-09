ddcdb810c8a2ab1811461ab18694c24f9bd1e0af
GuiBM04 <ggbm04@gmail.com>
2025-06-09 20:07:38 -0300

    iniciando projeto springboot

---
+++ src/main/java/com/api/pix_fraud/PixFraudApplication.java
@@ -0,0 +1,13 @@
+package com.api.pix_fraud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PixFraudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PixFraudApplication.class, args);
+	}
+
+}
